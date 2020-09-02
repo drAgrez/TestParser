f6735bc665b8e3a1eb4ad1ccaf68a7056bb05fd6
agrez <agrebanshee@mail.ru>
2020-09-02 15:02:21 +0300

    Добавьте файлы проекта.

---
+++ TestParser/Program.cs
@@ -0,0 +1,30 @@
+﻿using AngleSharp.Xml.Parser;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
+using System.Net.Http;
+
+namespace TestParser
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            using var client = new HttpClient();
+            var result = await client.GetStringAsync("https://yastatic.net/market-export/_/partner/help/YML.xml");
+
+            var parser = new XmlParser();
+            var offerID = parser
+                .ParseDocument(result)
+                .QuerySelectorAll("offer[id]");
+
+            using (StreamWriter sw = new StreamWriter("offerID.txt"))
+            {
+                foreach(var item in offerID)
+                {
+                    sw.WriteLine(item.GetAttribute("id"));
+                }
+            }
+        }
+    }
+}
